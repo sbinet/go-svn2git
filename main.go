fc62e34d8563bddca594f0ee173a70fa6d0ee757
Sebastien Binet <binet@cern.ch>
2012-10-05 15:23:08 +0200

    working prototype

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
+
+	"github.com/gonuts/iochan"
 )
 
 var g_verbose = flag.Bool("verbose", false, "")
@@ -46,6 +49,47 @@ func path_exists(name string) bool {
 	}
 	if os.IsNotExist(err) {
 		return false
+	}
+	return false
+}
+
+func print_cmd(cmd *exec.Cmd) {
+	if *g_verbose {
+		fmt.Printf(":: running %s\n", strings.Join(cmd.Args, " "))
+	}
+}
+
+func debug_cmd(cmd *exec.Cmd) {
+	if *g_verbose {
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+}
+
+func git_cmd(cmdargs ...string) []string {
+	lines := []string{}
+	cmd := exec.Command("git", cmdargs...)
+	if *g_verbose {
+		fmt.Printf(":: running %s\n", strings.Join(cmd.Args, " "))
+	}
+	out,err := cmd.CombinedOutput()
+	if err != nil {
+		// ignore error
+		return lines
+	}
+	r := bufio.NewReader(bytes.NewBuffer(out))
+	for line := range iochan.ReaderChan(r, "\n") {
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func is_in_slice(elmt string, slice []string) bool {
+	for _, v := range slice {
+		if v == elmt {
+			return true
+		}
 	}
 	return false
 }
@@ -172,24 +216,15 @@ func get_branches() error {
 	// ignore console color codes
 	cmd := exec.Command("git", "branch", "-l", "--no-color")
 	if *g_verbose {
+		fmt.Printf(":: --> building list of [local branches]...\n")
 		fmt.Printf(":: running %s\n", strings.Join(cmd.Args, " "))
-		// cmd.Stdin = os.Stdin
-		// cmd.Stdout = os.Stdout
-		// cmd.Stderr = os.Stderr
 	}
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
 	lines := bufio.NewReader(bytes.NewBuffer(out))
-	for {
-		line, err1 := lines.ReadString('\n')
-		if err1 != nil {
-			if err1 != io.EOF {
-				err = err1
-			}
-			break
-		}
+	for line := range iochan.ReaderChan(lines, "\n") {
 		if *g_verbose {
 			fmt.Printf("   %q\n", line)
 		}
@@ -206,10 +241,8 @@ func get_branches() error {
 	// remote branches...
 	cmd = exec.Command("git", "branch", "-r", "--no-color")
 	if *g_verbose {
+		fmt.Printf(":: --> building list of [remote branches]...\n")
 		fmt.Printf(":: running %s\n", strings.Join(cmd.Args, " "))
-		// cmd.Stdin = os.Stdin
-		// cmd.Stdout = os.Stdout
-		// cmd.Stderr = os.Stderr
 	}
 	out, err = cmd.CombinedOutput()
 	if err != nil {
@@ -238,9 +271,12 @@ func get_branches() error {
 	}
 
 	// tags are remote branches that start with "svn/tags/"
+	if *g_verbose {
+		fmt.Printf(":: --> building list of [svn-tags]...\n")
+	}
 	for _, branch := range g_repo.remote_branches {
 		if strings.HasPrefix(branch, "svn/tags/") {
-			tag := branch[len("svn/tags/"):]
+			tag := branch //branch[len("svn/tags/"):]
 			if *g_verbose {
 				fmt.Printf(":: adding tag %q...\n", tag)
 			}
@@ -379,21 +415,220 @@ func do_clone() error {
 
 func fix_tags() error {
 	var err error = nil
+	usr := make(map[string]string)
+
+	// we only change git config values if g_repo.tags are available.
+	// so it stands to reason we should revert them only in that case.
+	defer func() {
+		if len(g_repo.tags) == 0 {
+			return
+		}
+		for name,v := range usr {
+			vv := strings.Trim(v, " ")
+			if vv != "" {
+				cmd := exec.Command("git", "config", "--local", name, 
+					strconv.Quote(vv))
+				_ = cmd.Run()
+			} else {
+				cmd := exec.Command("git", "config", "--local", "--unset", name)
+				_ = cmd.Run()
+			}
+			//fmt.Printf("%s: %q %q\n", name, v, vv)
+		}
+	}()
+
+	git_cfg := func(k string) (string, error) {
+		cmd := exec.Command("git", "config", "--local", "--get", k)
+		out,err := cmd.CombinedOutput()
+		if err != nil {
+			// ignore error!
+			return "", nil
+		}
+		r := bufio.NewReader(bytes.NewBuffer(out))
+		lines := []string{}
+		for line := range iochan.ReaderChan(r, "\n") {
+			lines = append(lines, line)
+		}
+		if len(lines) != 1 {
+			return "", fmt.Errorf("too many lines (%d)", len(lines))
+		}
+		return lines[0], err
+	}
+	usr["user.name"], _ = git_cfg("user.name")
+	usr["user.email"], _ = git_cfg("user.name")
+
+	for itag, tag := range g_repo.tags {
+		tag = strings.Trim(tag, " ")
+		id := tag[len("svn/tags/"):]
+		if *g_verbose {
+			hdr := ""
+			if itag > 0 {
+				hdr = "\n"
+			}
+			fmt.Printf("%s:: processing svn tag [%s]...\n", hdr, tag)
+		}
+		subject := git_cmd("log", "-1", "--pretty=format:%s", tag)[0]
+		date := git_cmd("log", "-1", "--pretty=format:%ci", tag)[0]
+		author := git_cmd("log", "-1", "--pretty=format:%an", tag)[0]
+		email := git_cmd("log", "-1", "--pretty=format:%ae", tag)[0]
+
+		cmd := exec.Command("git", "config", "--local", "user.name",
+			"\""+author+"\"")
+		print_cmd(cmd)
+		_ = cmd.Run()
+
+		cmd = exec.Command("git", "config", "--local", "user.email",
+			"\""+email+"\"")
+		print_cmd(cmd)
+		_ = cmd.Run()
+
+		cmd = exec.Command("git", "tag", "-a", "-m",
+			fmt.Sprintf("\"%s\"", subject), 
+			id, 
+			tag)
+		cmd.Env = os.Environ()
+		cmd.Env = append(cmd.Env, fmt.Sprintf("GIT_COMMITTER_DATE=%s", date))
+		print_cmd(cmd)
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf("**error** %v\n", err)
+			return err
+		}
+
+		cmd = exec.Command("git", "branch", "-d", "-r", tag)
+		print_cmd(cmd)
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf("**error** %v\n", err)
+			return err
+		}
+
+		//fmt.Printf("tag: %q - subject: %q\n", tag, subject)
+	}
 	return err
 }
 
 func fix_branches() error {
 	var err error = nil
+	svn_branches := []string{}
+	for _, v := range g_repo.remote_branches {
+		if is_in_slice(v, g_repo.tags) {
+			if *g_verbose {
+				fmt.Printf("-- discard [%s]...\n", v)
+			}
+			continue
+		}
+		if strings.HasPrefix(v, "svn/") {
+			svn_branches = append(svn_branches, v)
+		}
+	}
+	if *g_verbose {
+		fmt.Printf("svn-branches: %v\n", svn_branches)
+	}
+
+	if *g_rebase {
+		cmd := exec.Command("git", "svn", "fetch")
+		print_cmd(cmd)
+		if *g_verbose {
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+		}
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, branch := range svn_branches {
+		branch = branch[len("svn/"):]
+		if *g_rebase && (is_in_slice(branch, g_repo.local_branches) || branch == "trunk") {
+			lbranch := branch
+			if branch == "trunk" {
+				lbranch = "master"
+			}
+			cmd := exec.Command("git", "checkout", "-f", lbranch)
+			print_cmd(cmd)
+			debug_cmd(cmd)
+			err = cmd.Run()
+			if err != nil {
+				return err
+			}
+
+			cmd = exec.Command("git", "rebase", 
+				fmt.Sprintf("remotes/svn/%s",branch),
+				)
+			print_cmd(cmd)
+			debug_cmd(cmd)
+			err = cmd.Run()
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		if branch == "trunk" || is_in_slice(branch, g_repo.local_branches) {
+			continue
+		}
+
+		cmd := exec.Command("git", "branch", "--track", branch,
+			fmt.Sprintf("remotes/svn/%s", branch))
+		print_cmd(cmd)
+		debug_cmd(cmd)
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
+
+		cmd = exec.Command("git", "checkout", branch)
+		print_cmd(cmd)
+		debug_cmd(cmd)
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
+	}
 	return err
 }
 
 func fix_trunk() error {
 	var err error = nil
+	trunk := ""
+	for _, v := range g_repo.remote_branches {
+		if strings.Trim(v, " ") == "trunk" {
+			trunk = "trunk"
+			break
+		}
+	}
+	var cmds []string
+	if trunk != "" && !*g_rebase {
+		cmds = []string{
+			"git checkout svn/trunk",
+			"git branch -D master",
+			"git checkout -f -b master",
+		}
+	} else {
+		cmds = []string{"git checkout -f master"}
+	}
+	for _, cmdstr := range cmds {
+		cmdargs := strings.Split(cmdstr, " ")
+		cmd := exec.Command(cmdargs[0], cmdargs[1:]...)
+		print_cmd(cmd)
+		debug_cmd(cmd)
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
+	}
 	return err
 }
 
 func optimize_repos() error {
 	var err error = nil
+	cmd := exec.Command("git", "gc")
+	print_cmd(cmd)
+	debug_cmd(cmd)
+	err = cmd.Run()
 	return err
 }
 
